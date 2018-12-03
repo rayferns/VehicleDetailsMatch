f52a91679208e32909c3f3ee6774f49d4e714c64
Server <rayferns@HP_Spectre_X360.mshome.net>
2018-12-03 00:02:27 +0000

    Added new java files and step definitions

--- TestProject/src/test/Features/VehicleDetailsValidation.feature
+++ TestProject/src/test/Features/VehicleDetailsValidation.feature
@@ -1,26 +1,26 @@
-Feature: Car Vehicle details validation
-
-I want to validate all car registration number against gov.uk services
-
 @SmokeTest
-Scenario Outline: Validate Vehicle details against valid car registration numbers
-Given I am on the https://vehicleenquiry.service.gov.uk/ page
-When I enter a valid <registrationnumber> of the vehicle
-And I confirm the <registration number>, <make> and <colour> is correct
-And I select 'Yes' to confirm they all match
-And I press continue
-Then I see the Vehicle details page matching with the relavant car registration details
+Feature: Use the https://www.gov.uk/get-vehicle-information-from-dvla site to verify matching information about a vehicle using the vehicle registration
+  As a user of the gov dvla site
+  I want to be able to enter a vehicle registraion number
+  and verify the details match that vehicle
+
+  Background: Get to the DVLA page where I can enter vehicle registration
+    Given I am on the gov dvla page
+    Then I can see the heading 'Get vehicle information from DVLA' on 'get information' page
+    When I click on the button labeled 'Start now' on 'get information' page
+    Then I land on the 'Enter the registration number of the vehicle' page with title like 'check if vehicle is taxed and had an MOT'
+
+  Scenario Outline: Verify gov dvla held vehicle information using a valid vehicle registration
+    When I enter a valid vehicle registration '<registrationnumber>' on 'vehicle enquiry' page
+    And I click on the button labeled '<button_label>' on 'vehicle enquiry' page
+    Then I land on the 'confirm vehicle' page with title like '<page_title>'
+    And I verify '<details>' of the vehicle on the 'confirm vehicle' page
+    When I click radio button '<radio_button>' on the confirm vehicle page
+    And I click on the button labeled '<string>' on 'confirm vehicle' page
+    Then I land on the 'view vehicle' page with title like '<page_title_2>'
+    And I verify the vehicle has Tax and MOT on the view vehicle page
+    And I verify '<further_details>' of the vehicle on the 'view vehicle' page
+
 Examples: Valid Car registration details
-|registrationnumber| Make | Color | 
-|SL67 HWH| TOYOTA | WHITE |
-|kX67 LFA| AUDI | BLACK |
-|KP18 ZNZ| VAUXHALL | RED |
-
-
-Scenario: Validate Vehicle details against invalid car registration numbers
-Given I am on the https://vehicleenquiry.service.gov.uk/ page
-When I enter an invalid <registration number> of the vehicle
-And I confirm the <registration number>, <make> and <colour> is correct
-And I select 'No' to search again
-And I press continue
-Then I am navigated to the previous page to enter the registration number of the vehicle
+| vehicl_reg | button_label | page_title | details | further_details  |
+| KX67 LFA    | Continue     | Check if a vehicle is taxed and has an MOT | Registration number:KX67 LFA, Make:AUDI, Color:BLACK | Date of first registration: September 2017, Year of manufacture: 2017, Fuel type: PETROL |
--- TestProject/src/test/utility/FileUtils.java
+++ TestProject/src/test/utility/FileUtils.java
@@ -1,47 +1,32 @@
-package test.java;
+package test.utility;
 
 import java.net.FileNameMap;
 import java.net.URLConnection;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.io.Writer;
 import java.util.concurrent.*;
-
+import java.util.stream.Stream;
 
 import org.apache.tika.Tika;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.digest.InputStreamDigester;
 
+/* Part 1 of the task
+ * 
+ * */
 public class FileUtils {
-	
-	
-//
-//	public static String getMimeType(String fileUrl)
-//		      throws java.io.IOException
-//		    {
-//		      FileNameMap fileNameMap = URLConnection.getFileNameMap();
-//		      String type = fileNameMap.getContentTypeFor(fileUrl);
-//
-//		      return type;
-//		    }
-//
-//		    public static void main(String args[]) throws Exception {
-//		      System.out.println(FileUtils.getMimeType("file://c:/temp/test.TXT"));
-//		      // output :  text/plain
-//	   }
 
-//	 static String inpath = "C:\\Container\\Automation\\Udemy_Training\\Selenium_with_Java\\Samples\\VehicleDetailsMatch\\TestProject\\src\\test\\resources";
-//	 static String outpath = "C:\\Container\\Automation\\Udemy_Training\\Selenium_with_Java\\Samples\\VehicleDetailsMatch\\TestProject\\src\\test\\resources\\output.txt";
-	static String inpath = "src\\test\\resources";
-	//static String outpath = "src\\test\\resources\\output.txt";
-	//static final Pattern PATTERN = Pattern.compile("(.*)\\.(.*)");
+	static String inpath = "src/test/resources";
 		
 	public static void main(String[]args) throws IOException{
 	        File Folder = new File(inpath);
@@ -50,33 +35,45 @@ public class FileUtils {
 	        if(files.length>1)
 	        {
 	            for(int i = 0;i<files.length; i++){
-	                //System.out.println("reading...");
 	                System.out.println(files[i]);
 	                Reader in = new FileReader(files[i]);
-	                
+	                long fileSizeInBytes = files[i].length();
 	                // Get File mime type
 	                Tika tika = new Tika();
 	                String mimeType = tika.detect(files[i]);
 	                System.out.println("mimeType: "+ mimeType);
 	                System.out.println("File path: "+ files[i].getAbsoluteFile());
 	                System.out.println("File extension: "+ getFileExtension(files[i]));
-	                System.out.println("File name: "+ files[i].getName() + '\n');
-	                
-	                
-	                in.close();
-	                //out.close(); 
+	                System.out.println("File name: "+ files[i].getName());
+	                System.out.println("File Size: "+ fileSizeInBytes / 1024 + '\n');
+               	    	                
+	                in.close(); 
 	            }
 	        }
 	        else{
 	            System.out.println("found only one file...");
 	            System.out.println(files);
 	        }
+	        
+	    	final File folder = new File(inpath);
+	    	listFilesForFolder(folder);
 
-	 }
+}
 	private static String getFileExtension(File file) {
 		String fileName = file.getName();
         if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
         return fileName.substring(fileName.lastIndexOf(".")+1);
-        else return "";
+        else return "";   
+        
+	}
+	public static void listFilesForFolder(final File folder) {
+	    for (final File fileEntry : folder.listFiles()) {
+	        if (fileEntry.isDirectory()) {
+	            listFilesForFolder(fileEntry);
+	        } else {
+	            System.out.println(fileEntry.getName());
+	            
+	        }
+	    }
 	}
 }
